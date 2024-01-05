725be2752d122997697ea18b336a8996a99f4771
SamOstrander <samostr@gmail.com>
2024-01-04 19:42:38 -0600

    split into functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import axios from 'axios'; 
+//import Login from '../components/Login.js';
 import './App.css';
 
 class App extends React.Component { 
@@ -13,24 +14,8 @@ class App extends React.Component {
     }
 
     //I know I did this part, but I would redo it like:
-    // getTests() {
-    //   let data ;
-    //   axios.get('http://localhost:8000/testing/') 
-    //   .then(res => { 
-    //     data = res.data; 
-    //     this.setState({ 
-    //         details : data     
-    //     });
-    //   }) 
-    //   .catch(err => {})
-    // }
-
-    // componentDidMount() {
-    //    this.getTests()
-    // }
-
-    componentDidMount() {
-      let data ; 
+    getTests() {
+      let data ;
       axios.get('http://localhost:8000/testing/') 
       .then(res => { 
         data = res.data; 
@@ -38,8 +23,24 @@ class App extends React.Component {
             details : data     
         });
       }) 
-      .catch(err => {}) 
-    } 
+      .catch(err => {})
+    }
+
+    componentDidMount() {
+       this.getTests()
+    }
+
+    // componentDidMount() {
+    //   let data ; 
+    //   axios.get('http://localhost:8000/testing/') 
+    //   .then(res => { 
+    //     data = res.data; 
+    //     this.setState({ 
+    //         details : data     
+    //     });
+    //   }) 
+    //   .catch(err => {}) 
+    // } 
 
     handleInput = (e) => { 
       this.setState({ 
@@ -50,48 +51,50 @@ class App extends React.Component {
     // as a best practice I would probably put axios.post in its own function then just call it in the handle submit
 
     // EXAMPLE:
-    // handleCreateTest = (newTest) => {
-    //   axios.post("http://localhost:8000/testing/", newTest)
-    //   .then((res) => { 
-    //     this.componentDidMount();
-    //     OR IF EARLIER CODE CHANGED: this.getTests();
-    //   })
-    //   .catch((err) => {})
-    // }
+    handleCreateTest = (newTest) => {
+      axios.post("http://localhost:8000/testing/", {
+        name:newTest.name,
+        number:newTest.number,
+      })
+      .then((res) => { 
+        this.getTests();
+      })
+      .catch((err) => {})
+    }
 
   handleSubmit = (e) => {
     e.preventDefault();
     // Then call here:
-    // this.handleCreateTest(this.state)
-    // this.setState({ 
-    //         name: "", 
-    //         number: "", 
-    //       });
+    this.handleCreateTest(this.state)
+    this.setState({ 
+            name: "", 
+            number: "", 
+          });
     //If you uncomment above, then comment out below
-    let data ; 
+    // let data ; 
 
-    axios.post("http://localhost:8000/testing/", { 
-      name: this.state.name, 
-      number: this.state.number, 
-    })
-    .then((res) => { 
-      this.setState({ 
-        name: "", 
-        number: "", 
-      }); 
-    }) 
-    .catch((err) => {})
+    // axios.post("http://localhost:8000/testing/", { 
+    //   name: this.state.name, 
+    //   number: this.state.number, 
+    // })
+    // .then((res) => { 
+    //   this.setState({ 
+    //     name: "", 
+    //     number: "", 
+    //   }); 
+    // }) 
+    // .catch((err) => {})
 
-    // get data to reflect updated database
-    axios.get('http://localhost:8000/testing/') 
-    .then(res => { 
-      data = res.data; 
-      this.setState({ 
-            details : data     
-      }); 
-    }) 
-    .catch(err => {}) 
-    //TO HERE
+    // // get data to reflect updated database
+    // axios.get('http://localhost:8000/testing/') 
+    // .then(res => { 
+    //   data = res.data; 
+    //   this.setState({ 
+    //         details : data     
+    //   }); 
+    // }) 
+    // .catch(err => {}) 
+    // //TO HERE
   }
   
   render() { 
